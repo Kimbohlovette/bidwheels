5b98eed6338f0eea4d5f3f45a8718d1285eb3fb1
KimbohLovette <kimbohlovette@gmail.com>
2023-04-02 21:40:57 +0100

    Hero section completed on desktop

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,62 +1,58 @@
-import React from "react";
-import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
-import { BsStarFill, BsStarHalf } from "react-icons/bs";
-import Car from "../components/Car";
-
-const whoweare = require("../assets/whoweare.png");
-const bgImage = require('../assets/hero-bg-image1.jpg');
+import React from 'react';
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
+import { BsStarFill, BsStarHalf } from 'react-icons/bs';
+import Car from '../components/Car';
 
 const Home = () => {
+	const whoweare = require('../assets/whoweare.png');
+	const bgImage = require('../assets/hero-bg-image2.jpg');
 	const cars = [
 		{
-			image: require("../assets/m23.jpeg"),
-			name: "2023 Mini Coper",
+			image: require('../assets/m23.jpeg'),
+			name: '2023 Mini Coper',
 			description:
 				"The iconic mini coper has grown both larger and more advanced over the years, but it's still a small, agile car, with very attractive styling for 2023",
 		},
 		{
-			image: require("../assets/t1.jpeg"),
-			name: "2023 Toyota Bz4X Review",
+			image: require('../assets/t1.jpeg'),
+			name: '2023 Toyota Bz4X Review',
 			description:
-				"Toyota finally enters the Ev game with a small crossover aimed at the heart of the market",
+				'Toyota finally enters the Ev game with a small crossover aimed at the heart of the market',
 		},
 		{
-			image: require("../assets/b1.jpeg"),
-			name: "Bentley Bentayga",
+			image: require('../assets/b1.jpeg'),
+			name: 'Bentley Bentayga',
 			description:
-				"This is a mid-size luxury crossover SUV produced by Bentley Motors since the late 2015s",
+				'This is a mid-size luxury crossover SUV produced by Bentley Motors since the late 2015s',
 		},
 		{
-			image: require("../assets/m1.jpeg"),
-			name: "Mercedez-Bend",
+			image: require('../assets/m1.jpeg'),
+			name: 'Mercedez-Bend',
 			description:
-				"The Mercedes-Benz CIS-class is a form-first art piece that just so happens to provide the practicality of a chic four-door Sedan. View 2023 CIs",
+				'The Mercedes-Benz CIS-class is a form-first art piece that just so happens to provide the practicality of a chic four-door Sedan. View 2023 CIs',
 		},
 	];
 	return (
-		<div className="min-h-screen">
-			<section
-				className="hero h-[70vh] flex items-center object-center object-cover"
-				style={{ backgroundImage: `url(${bgImage})`, objectPosition: 'center', objectFit: 'cover', height: '100%' }}
-			>
-				<div className="w-full flex flex-col items-center sm:flex-row">
-					<div className="caption flex-1">
-						<h1 className="text-5xl font-bold">
-							Welcome Iknite Space
+		<div className="min-h-screen w-full">
+			<section className="relative h-[80vh] flex items-center">
+				<img
+					src={bgImage}
+					alt=""
+					className="top-0 left-0 w-full h-full -z-10 object-[(50%,_80%)] aspect-video"
+				/>
+				<div className="w-full p-8 flex flex-col items-center sm:flex-row absolute top-0 left-0 h-full bg-primary/40">
+					<div className="caption flex-1 max-w-1/2">
+						<h1 className="text-5xl font-bold text-slate-50 w-1/2 mx-auto">
+							Buy your dream car at your own budget.
 						</h1>
 					</div>
-					<div className="action-card flex-1 py-16 px-8 text-white bg-primary/60">
-						<p>
+					<div className="action-card flex-1 py-16 px-8 bg-primary/80 max-w-lg">
+						<p className="text-slate-100 text-sm">
 							Shop the latest iPhone models and accessroies, Save
 							with Apple Trade-In, carrier offers, and flexible
 							monthly payment options. Get expert help today.
 						</p>
-						<p>
-							Shop the latest iPhone models and accessroies, Save
-							with Apple Trade-In, carrier offers, and flexible
-							monthly payment options. Get expert help today.
-						</p>
-						<button className="button w-48 text-primary bg-white border mt-8">
+						<button className="button w-48 text-primary bg-white border mt-8 font-semibold text-lg">
 							BUY
 						</button>
 					</div>
@@ -85,29 +81,40 @@ const Home = () => {
 					</h2>
 					<div className="py-4 pr-4">
 						<p className="text-primary">
-							We are involved in the selling of cars. We partner with
-							individuals who import cars for sale, put these cars up for
-							bidding, and sell them to the highest bidder. We ensure customers'
-							satisfication by promoting only high quality goods, and by making
-							sure our partners are legit and reliable. We help shopers find
-							great car deals and connect with top rated dealers. Bid 4 Wheels
-							offers a pre-owned inventory that ranges from economic cars to
-							luxury vehicles at affordable price with updates on the latest
-							cars available. We strive to find you the vehicle you've been
-							looking for, and make the process easy for you. We help our
-							partners make quick sales with little effort by puttin up their
-							products for automatic bidding, hence saving them the stress
+							We are involved in the selling of cars. We partner
+							with individuals who import cars for sale, put these
+							cars up for bidding, and sell them to the highest
+							bidder. We ensure customers' satisfication by
+							promoting only high quality goods, and by making
+							sure our partners are legit and reliable. We help
+							shopers find great car deals and connect with top
+							rated dealers. Bid 4 Wheels offers a pre-owned
+							inventory that ranges from economic cars to luxury
+							vehicles at affordable price with updates on the
+							latest cars available. We strive to find you the
+							vehicle you've been looking for, and make the
+							process easy for you. We help our partners make
+							quick sales with little effort by puttin up their
+							products for automatic bidding, hence saving them
+							the stress
 						</p>
 					</div>
 				</div>
 			</section>
 			<section className="flex flex-row px-8 py-8 justify-between items-start">
 				<div className="flex-1">
-					<h2 className="text-primary font-semibold text-2xl pb-6">Rating</h2>
+					<h2 className="text-primary font-semibold text-2xl pb-6">
+						Rating
+					</h2>
 					<div className="flex flex-row items-center justify-between w-[60%] pb-3">
-						{Array.from({ length: 4 }, (item, index) => index).map((_, i) => (
-							<BsStarFill className="text-yellow-500 text-3xl" key={i} />
-						))}
+						{Array.from({ length: 4 }, (item, index) => index).map(
+							(_, i) => (
+								<BsStarFill
+									className="text-yellow-500 text-3xl"
+									key={i}
+								/>
+							)
+						)}
 						<BsStarHalf className="text-yellow-500 text-3xl" />
 					</div>
 					<h4 className="text-primary font-semibold text-sm">
@@ -120,15 +127,23 @@ const Home = () => {
 							Quality
 						</h2>
 						<div className="flex flex-row w-[50%]">
-							{Array.from({ length: 5 }, (item, index) => index).map((_, i) => (
+							{Array.from(
+								{ length: 5 },
+								(item, index) => index
+							).map((_, i) => (
 								<div className="flex-1 mr-[1px] bg-primary h-3" />
 							))}
 						</div>
 					</div>
 					<div className="mb-8">
-						<h2 className="text-primary font-semibold text-2xl mb-4">Value</h2>
+						<h2 className="text-primary font-semibold text-2xl mb-4">
+							Value
+						</h2>
 						<div className="flex flex-row w-[50%]">
-							{Array.from({ length: 5 }, (item, index) => index).map((_, i) => (
+							{Array.from(
+								{ length: 5 },
+								(item, index) => index
+							).map((_, i) => (
 								<div className="flex-1 mr-[1px] bg-primary h-3" />
 							))}
 						</div>
@@ -138,7 +153,10 @@ const Home = () => {
 							Credibility
 						</h2>
 						<div className="flex flex-row w-[50%]">
-							{Array.from({ length: 5 }, (item, index) => index).map((_, i) => (
+							{Array.from(
+								{ length: 5 },
+								(item, index) => index
+							).map((_, i) => (
 								<div className="flex-1 mr-[1px] bg-primary h-3" />
 							))}
 						</div>
@@ -180,12 +198,10 @@ const Home = () => {
 					</div>
 				</div>
 				<div className="flex flex-row flex-wrap justify-center gap-16 [&>*]:flex-1">
-					{
-						cars.map((car) => {
-							console.log('car: ', car)
-							return <Car car={car} />
-						})
-					}
+					{cars.map((car) => {
+						console.log('car: ', car);
+						return <Car car={car} />;
+					})}
 				</div>
 			</section>
 			<section></section>
