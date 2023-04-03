775949663c062c24e991f9aeb5f10c720205157b
Rouclec <senatorasonganyi97@gmail.com>
2023-04-03 05:00:48 +0100

    shop working

--- src/app/pages/Shop.tsx
+++ src/app/pages/Shop.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
-import Product from '../components/Product';
+import React, { useEffect, useState } from "react";
+import Product from "../components/Product";
+import { ProductType } from "../types";
+import axios from "axios";
+import { base_url } from "../App";
+import { Link } from "react-router-dom";
 
 const Shop = () => {
+	const [products, setProducts] = useState<ProductType[]>([]);
+	useEffect(() => {
+		const getProducts = async () => {
+			const productsFound = await axios.get(`${base_url}/product`);
+			setProducts(productsFound.data.data);
+			localStorage.setItem(
+				"@products",
+				JSON.stringify(productsFound.data.data)
+			);
+		};
+		const loadProduct = async () => {
+			const productsString = localStorage.getItem("@products");
+			if (productsString) {
+				const localProducts = JSON.parse(productsString);
+				if (localProducts.length > 0) {
+					setProducts(localProducts);
+				} else {
+					getProducts();
+				}
+			} else {
+				getProducts();
+			}
+		};
+		loadProduct();
+	}, []);
 	return (
 		<div className="min-h-screen">
 			<section className="relative h-[75vh] flex items-center">
@@ -16,9 +45,9 @@ const Shop = () => {
 							Let Us Help You Find Your Dream Ride
 						</h1>
 						<p className="text-slate-100 text-sm">
-							Shop the latest car models and accessories, Save
-							with Bid 4 Wheels Trade-In, carrier offers, and
-							flexible payment options. Get expert help today.
+							Shop the latest car models and accessories, Save with Bid 4 Wheels
+							Trade-In, carrier offers, and flexible payment options. Get expert
+							help today.
 						</p>
 					</div>
 				</div>
@@ -31,14 +60,8 @@ const Shop = () => {
 					<div className="flex flex-row">
 						<div className="border p-16 max-w-sm [&_select]:w-full [&_select]:rounded-sm [&_select:focus]:border-primary [&_select:focus]:shadow-sm [&_select:focus]:shadow-primary-light">
 							<div>
-								<h1 className="text-4xl text-primary font-bold my-4">
-									Price
-								</h1>
-								<select
-									className="form-select my-4"
-									name="price"
-									id="price"
-								>
+								<h1 className="text-4xl text-primary font-bold my-4">Price</h1>
+								<select className="form-select my-4" name="price" id="price">
 									<option value="0">Filter by price</option>
 									<option value="1">20,0000000</option>
 									<option value="2">10,0000000</option>
@@ -46,14 +69,8 @@ const Shop = () => {
 								</select>
 							</div>
 							<div>
-								<h1 className="text-4xl text-primary font-bold my-4">
-									Model
-								</h1>
-								<select
-									className="form-select my-4"
-									name="model"
-									id="model"
-								>
+								<h1 className="text-4xl text-primary font-bold my-4">Model</h1>
+								<select className="form-select my-4" name="model" id="model">
 									<option value="0">Filter by model</option>
 									<option value="1">M1</option>
 									<option value="2">M2</option>
@@ -64,37 +81,29 @@ const Shop = () => {
 								<h1 className="text-4xl text-primary font-bold my-4">
 									Fuel Type
 								</h1>
-								<select
-									className="form-select my-4"
-									name="price"
-									id="price"
-								>
-									<option value="0">
-										Filter by fuel type
-									</option>
+								<select className="form-select my-4" name="price" id="price">
+									<option value="0">Filter by fuel type</option>
 									<option value="1">F1</option>
 									<option value="2">F2</option>
 									<option value="3">F3</option>
 								</select>
 							</div>
 							<div>
-								<h1 className="text-4xl text-primary font-bold my-4">
-									Engine
-								</h1>
-								<select
-									className="form-select my-4"
-									name="price"
-									id="price"
-								>
+								<h1 className="text-4xl text-primary font-bold my-4">Engine</h1>
+								<select className="form-select my-4" name="price" id="price">
 									<option value="0">Filter by Engine</option>
 									<option value="1">V1</option>
 									<option value="2">W1</option>
 									<option value="3">V7</option>
 								</select>
-							</div>{' '}
+							</div>{" "}
 						</div>
-						<div>
-							<Product />
+						<div className="grid grid-cols-2 md:grid-cols-3 gap-8 px-8">
+							{products.map((product) => (
+								<Link to={`/product/${product._id}`} state={product}>
+									<Product product={product} />
+								</Link>
+							))}
 						</div>
 					</div>
 				</div>
