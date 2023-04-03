e7e1ef8a0247326a8d6314006112b0f58c4a73cd
Rouclec <senatorasonganyi97@gmail.com>
2023-04-03 05:11:56 +0100

    shop carousel

--- src/app/pages/Shop.tsx
+++ src/app/pages/Shop.tsx
@@ -5,6 +5,8 @@ import axios from "axios";
 import { base_url } from "../App";
 import { Link } from "react-router-dom";
 
+const bgImage = require('../assets/shop-hero.jpg')
+
 const Shop = () => {
 	const [products, setProducts] = useState<ProductType[]>([]);
 	useEffect(() => {
@@ -35,9 +37,9 @@ const Shop = () => {
 		<div className="min-h-screen">
 			<section className="relative h-[75vh] flex items-center">
 				<img
-					// src={bgImage}
+					src={bgImage}
 					alt=""
-					className="top-0 left-0 w-full h-full -z-10 object-[(50%,_80%)] aspect-video"
+					className="top-0 left-0 w-full h-full -z-10 object-cover object-center aspect-video"
 				/>
 				<div className="w-full p-8 flex flex-col justify-center items-center sm:flex-row absolute top-0 left-0 h-full bg-primary/40">
 					<div className="action-card flex-1 py-16 px-8 max-w-lg">
@@ -58,9 +60,9 @@ const Shop = () => {
 						Filter Products
 					</h1>
 					<div className="flex flex-row">
-						<div className="border p-16 max-w-sm [&_select]:w-full [&_select]:rounded-sm [&_select:focus]:border-primary [&_select:focus]:shadow-sm [&_select:focus]:shadow-primary-light">
+						<div className="border p-8 max-w-lg [&_select]:w-full [&_select]:rounded-sm [&_select:focus]:border-primary [&_select:focus]:shadow-sm [&_select:focus]:shadow-primary-light">
 							<div>
-								<h1 className="text-4xl text-primary font-bold my-4">Price</h1>
+								<h1 className="text-3xl text-primary font-bold my-4">Price</h1>
 								<select className="form-select my-4" name="price" id="price">
 									<option value="0">Filter by price</option>
 									<option value="1">20,0000000</option>
@@ -69,7 +71,7 @@ const Shop = () => {
 								</select>
 							</div>
 							<div>
-								<h1 className="text-4xl text-primary font-bold my-4">Model</h1>
+								<h1 className="text-3xl text-primary font-bold my-4">Model</h1>
 								<select className="form-select my-4" name="model" id="model">
 									<option value="0">Filter by model</option>
 									<option value="1">M1</option>
@@ -78,7 +80,7 @@ const Shop = () => {
 								</select>
 							</div>
 							<div>
-								<h1 className="text-4xl text-primary font-bold my-4">
+								<h1 className="text-3xl text-primary font-bold my-4">
 									Fuel Type
 								</h1>
 								<select className="form-select my-4" name="price" id="price">
@@ -89,7 +91,7 @@ const Shop = () => {
 								</select>
 							</div>
 							<div>
-								<h1 className="text-4xl text-primary font-bold my-4">Engine</h1>
+								<h1 className="text-3xl text-primary font-bold my-4">Engine</h1>
 								<select className="form-select my-4" name="price" id="price">
 									<option value="0">Filter by Engine</option>
 									<option value="1">V1</option>
